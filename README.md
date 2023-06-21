6396d694e1acba71aa96c6b5d3ad80456993c996
Tobias Schmidt <tobias2.schmidt@ukr.de>
2023-06-21 22:58:55 +0200

    Initial version.

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
-# toscs-vscode-commands
+# vstosc
 
-Private VSCode commands of tosc.
+VSCode commands from tosc.
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
 
 ## Commands
 
-*KnitRmd*: sends the following text to the currently active console:
+### KnitRmd:
+
+Sends the following text to the currently active console:
 
 ```R
 if (exists("params")) rm(params)
 rmarkdown::render("<path-of-currently-active-file")
 ```
-# toscvs
+## Contribute
+
+1. Clone this repo and open the folder in VSCode
+2. Modify [package.json](package.json) and [src/extensions.ts](src/extensions.ts) as required (ChatGPT is your friend).
+3. Hit F5 to and run the extension in a new *Extension Development Host* window.
+4. See [Debugging the extension](https://code.visualstudio.com/api/get-started/your-first-extension#debugging-the-extension) in case something isn't working as expected.
+5. 
+
+For further guidelines see [VSCode's Extension Docs](https://code.visualstudio.com/api)
---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+import * as vscode from 'vscode';
+
+const knitRmd = function () {
+	let terminal = vscode.window.activeTerminal;
+	let editor = vscode.window.activeTextEditor;
+	if (terminal !== undefined && editor !== undefined) {
+		let path = editor.document.fileName.replace(/\\/gi, "/");
+		terminal.sendText(`rmarkdown::render("${path}")`);
+	}
+}
+
+const mathMode = function () {
+	const editor = vscode.window.activeTextEditor;
+	if (editor) {
+		const currentPosition = editor.selection.active;
+        const lineText = editor.document.lineAt(currentPosition.line).text;
+		let insertText = '~$$';
+		let deleteRange: vscode.Range | undefined;
+		if (currentPosition.character > 0 && lineText.charAt(currentPosition.character - 1) === ' ') {
+			deleteRange = new vscode.Range(currentPosition.with(currentPosition.line, currentPosition.character - 1), currentPosition);
+		}
+		editor.edit((editBuilder) => {
+			if (deleteRange) {
+				editBuilder.delete(deleteRange);
+			}
+			editBuilder.insert(currentPosition, insertText);
+		}).then(() => {
+            const newPosition = new vscode.Position(currentPosition.line, currentPosition.character + 2);
+            editor.selection = new vscode.Selection(newPosition, newPosition);
+        });
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let registrationObjs = [
+		vscode.commands.registerCommand('vstosc.knitRmd', knitRmd),
+		vscode.commands.registerTextEditorCommand('vstosc.mathMode', mathMode),
+	]
+	context.subscriptions.push(...registrationObjs);
+}
+
+export function deactivate() { }
